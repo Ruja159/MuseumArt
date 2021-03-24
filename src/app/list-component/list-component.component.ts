6fa2c2dab76967587116f6a17c1463e098cebb80
Aleksandar Rujevic <ruja_prijedor@hotmail.com>
2021-03-24 12:33:58 +0100

    Readme commit

--- src/app/list-component/list-component.component.ts
+++ src/app/list-component/list-component.component.ts
@@ -25,8 +25,7 @@ interface AllCollections {
   styleUrls: ['./list-component.component.css'],
 })
 export class ListComponentComponent implements OnInit {
-
-  radioValue= '';
+  radioValue = '';
   public valueOfNode = '';
 
   public tempCollection;
@@ -42,10 +41,10 @@ export class ListComponentComponent implements OnInit {
 
   dataSource = new MatTreeNestedDataSource<Collections>();
 
-
-  constructor(private service: GetMethodService ,
+  constructor(
+    private service: GetMethodService,
     private comunicationService: ComunicationService
-    ) {}
+  ) {}
 
   ngOnInit(): void {
     this.service.getCollections().subscribe((data: any) => {
@@ -59,44 +58,43 @@ export class ListComponentComponent implements OnInit {
       localStorage.getItem('collections') || '{}'
     );
 
-    this.comunicationService.sendNewCollections$.subscribe((data) =>{
-      this.newCollection = data
-    })
- 
+    this.comunicationService.sendNewCollections$.subscribe((data) => {
+      this.newCollection = data;
+    });
   }
 
-  clickEvent( data) {
-    this.radioValue=data;
-    switch(data) {
-      case 'Painting': 
-        this.tempCollection = JSON.parse(JSON.stringify(this.collections)) // make deep clone of array;
-        this.tempCollection.forEach(museum => {
-          museum.collection.forEach(department => {
-            department.collection = department.collection.filter(i => i.type == 'painting');
-          })
+  clickEvent(data) {
+    this.radioValue = data;
+    switch (data) {
+      case 'Painting':
+        this.tempCollection = JSON.parse(JSON.stringify(this.collections)); // make deep clone of array;
+        this.tempCollection.forEach((museum) => {
+          museum.collection.forEach((department) => {
+            department.collection = department.collection.filter(
+              (i) => i.type == 'painting'
+            );
+          });
         });
 
         this.dataSource.data = this.tempCollection;
         break;
       case 'Potery':
         this.tempCollection = JSON.parse(JSON.stringify(this.collections)); //make deep clone of aray
-        this.tempCollection.forEach(museum => {
-          museum.collection.forEach(department => {
-            department.collection = department.collection.filter(i => i.type == 'potery');
-          })
+        this.tempCollection.forEach((museum) => {
+          museum.collection.forEach((department) => {
+            department.collection = department.collection.filter(
+              (i) => i.type == 'potery'
+            );
+          });
         });
 
         this.dataSource.data = this.tempCollection;
         break;
       default:
-        if(this.newCollection) {
-
-          this.dataSource.data = this.newCollection
-          this.tempCollection = this.newCollection
-
-        }
-        else {
-
+        if (this.newCollection) {
+          this.dataSource.data = this.newCollection;
+          this.tempCollection = this.newCollection;
+        } else {
           this.dataSource.data = this.collections;
           this.tempCollection = this.collections;
         }
@@ -107,13 +105,11 @@ export class ListComponentComponent implements OnInit {
   onSave(data) {
     this.valueOfNode = data;
 
-    this.comunicationService.sendId(data)
-
+    this.comunicationService.sendId(data);
   }
 
   hasChild = (_: number, node: Collections) =>
     !!node.collection && node.collection.length > 0;
-
 
   onSearchChange(event) {
     this.searchText = event.target.value;
@@ -123,12 +119,14 @@ export class ListComponentComponent implements OnInit {
   doSearch() {
     if (this.searchText) {
       this.tempCollection = JSON.parse(JSON.stringify(this.tempCollection)); //make deep clone of aray
-      this.tempCollection.forEach(museum => {
-        museum.collection.forEach(department => {
-          department.collection = department.collection.filter(i => i.name.toLowerCase().includes(this.searchText.toLowerCase()));
-        })
+      this.tempCollection.forEach((museum) => {
+        museum.collection.forEach((department) => {
+          department.collection = department.collection.filter((i) =>
+            i.name.toLowerCase().includes(this.searchText.toLowerCase())
+          );
+        });
       });
-      this.dataSource.data = this.tempCollection
+      this.dataSource.data = this.tempCollection;
     }
   }
 }
